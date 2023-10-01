1af9ce9e7267f9a59d73b7180391045624bf48a2
dazec <dasecojoshua@gmail.com>
2023-09-30 22:03:41 -0800

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { FaReact } from 'react-icons/fa';
+import './css/Tailwind.css';
 
 function App() {
   return (
-    <div className="">
-      
+    <div className="bg-red-500">
+      <h1 className='text-whte'>Welcome to My React App <FaReact /></h1>
     </div>
   );
 }
